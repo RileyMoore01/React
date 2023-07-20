c47b5d0cfb6b0e4306b1485a361e190c8eb69979
RileyMoore01 <xrileyxmoorex@outlook.com>
2023-07-20 09:47:19 -0500

    running build

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -23,19 +23,3 @@ function App() {
 }
 
 export default App;
-
-
-// function MyButton() {
-//   return (
-//       <button>Submit</button>
-//   );
-// }
-
-// export default function MyApp() {
-//   return (
-//       <div>
-//           <h1>Welcome to my app</h1>
-//           <MyButton />
-//       </div>
-//   );
-// }
